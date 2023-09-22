1d01f222a976957bcde9c26b4648b4beed887fd1
grubeng <61166458+grubeng@users.noreply.github.com>
2023-09-22 20:46:07 +0200

    Changed file extensions

--- tissuumaps/static/webpack.config.js
+++ tissuumaps/static/webpack.config.js
@@ -1,7 +1,21 @@
+const path = require("path");
+
 module.exports = {
   entry: {
-    index: "./js/index.js",
+    index: "./js/index.ts",
   },
-  output: { filename: "main.js" },
-  devtool: 'inline-source-map'
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: { loader: "ts-loader", options: { transpileOnly: true } },
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
